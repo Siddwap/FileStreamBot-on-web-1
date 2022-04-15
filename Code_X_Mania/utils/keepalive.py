e53f882e2f688d06246fc8136cff488468b4813c
Vivek Tomar <86093210+im-vivektomar@users.noreply.github.com>
2022-04-15 10:28:25 +0530

    Add files via upload

---
+++ Code_X_Mania/utils/keepalive.py
@@ -0,0 +1,22 @@
+# (c) ChVivekTomar
+
+import asyncio
+import logging
+import aiohttp
+import traceback
+from Code_X_Mania.vars import Var
+
+async def ping_server():
+    sleep_time = 1200
+    while True:
+        await asyncio.sleep(sleep_time)
+        try:
+            async with aiohttp.ClientSession(
+                timeout=aiohttp.ClientTimeout(total=10)
+            ) as session:
+                async with session.get(Var.URL) as resp:
+                    logging.info("Pinged server with response: {}".format(resp.status))
+        except TimeoutError:
+            logging.warning("Couldn't connect to the site URL..!")
+        except Exception:
+            traceback.print_exc()
